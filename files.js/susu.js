0c2956288046ddeec5cfc17d52678a7f9f18e383
Elizabeth1047 <egharevbalizzy5@gmail.com>
2022-07-27 09:34:35 -0700

    changes

---
+++ files.js/susu.js
@@ -0,0 +1,7 @@
+let hambuger = document.getElementById("ham");
+let ham = document.getElementById("ham");
+hambuger.addEventListener(click, function show(eve) {
+ evt.preventDefault;
+ ham.classList.add("reveal");
+ 
+});
