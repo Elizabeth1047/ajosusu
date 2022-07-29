5317fd60414a59b6dfc37d3e0aeb29db8481add3
Elizabeth1047 <egharevbalizzy5@gmail.com>
2022-07-29 11:34:45 -0700

    final editing

--- files.js/susu.js
+++ files.js/susu.js
@@ -1,7 +1,7 @@
 let hambuger = document.getElementById("ham");
-let ham = document.getElementById("ham");
-hambuger.addEventListener(click, function show(eve) {
- evt.preventDefault;
- ham.classList.add("reveal");
- 
+let ham = document.getElementById("list");
+hambuger.addEventListener("click", (eve) => {
+   
+ham.classList.toggle("reveal")
+ ham.classList.toggle("slid");
 });
